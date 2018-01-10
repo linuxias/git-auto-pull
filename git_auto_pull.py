a969ccda089cd05b6c22dbd4c37078dc30b3d4c8
Seungha Son <linuxias@gmail.com>
2018-01-10 13:00:08 +0900

    Add checkout method

--- git_auto_pull.py
+++ git_auto_pull.py
@@ -12,28 +12,42 @@ def is_git_repo(path):
         return False
 
 
-def get_branch_name(path):
-    old_path = os.getcwd()
-    os.chdir(path)
+def get_branch_name():
     active_branch = subprocess.check_output(["git", "rev-parse", "--abbrev-ref", "HEAD"])
-    os.chdir(old_path)
-    return active_branch
+    return active_branch.strip()
+
 
 def print_usage():
     print("Usage: auto_pull.py [-b <branch name>] [-h | --help] [-p | --path <path>]")
     sys.exit()
 
 
-def search(dirname):
+def run(dirname, branch):
     filenames = os.listdir(dirname)
     for filename in filenames:
         fullpath = os.path.join(dirname, filename)
         if os.path.isdir(fullpath):
             if (is_git_repo(fullpath)):
                 print(fullpath + " is git repo")
-                print(get_branch_name(fullpath))
+                old_path = os.getcwd()
+                os.chdir(fullpath)
+                current_branch = get_branch_name()
+                if (not current_branch == branch):
+                    print(not current_branch is branch)
+                    print(current_branch)
+                    print(branch)
+                    git_cmd_checkout(fullpath, branch)
+                os.chdir(old_path)
             else:
                 search(fullpath)
+
+
+def git_cmd_checkout(repo_path, branch):
+    print(repo_path)
+    g = git.Git(repo_path)
+    g.init()
+    g.checkout(branch)
+
 
 def main(argv):
     branch = ''
@@ -58,7 +72,7 @@ def main(argv):
                 print("Unknown option: " + opt)
                 print_usage()
 
-    search(current_path)
+    run(current_path, branch)
 
 if __name__=="__main__":
     main(sys.argv[1:])
